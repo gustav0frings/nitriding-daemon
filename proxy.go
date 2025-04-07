6e2adae36891aec2283a29aba2023115df130a84
gustav0 <gustav0@frings.ai>
2025-04-06 22:17:04 -0600

    add log

--- proxy.go
+++ proxy.go
@@ -24,6 +24,7 @@ var (
 // runNetworking calls the function that sets up our networking environment.
 // If anything fails, we try again after a brief wait period.
 func runNetworking(c *Config, stop chan struct{}) {
+	fmt.Println("Running networking")
 	var err error
 	for {
 		if err = setupNetworking(c, stop); err == nil {
